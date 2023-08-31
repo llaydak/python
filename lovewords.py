f5a204cf176eb2c4b5420010c1689ee1ae1d3e07
Ilayda <115037615+llaydak@users.noreply.github.com>
2023-08-31 22:07:04 +0300

    Update lovewords.py

--- lovewords.py
+++ lovewords.py
@@ -1,14 +1,16 @@
+#created a dictionary for the most common love words we use in our relationship.
 lovewords={"british":["love","honey","darling"],
            "turkish":["balım","canım","aşkım","bir tanem"],
            "tagalog":["mahal","sinta"],
            }
 print("love words dictionary of keanne and ilayda")
+#printed a simple title, then had it printed in an ordered way.
 for dd in lovewords:
     print(dd)
     for aa in lovewords[dd]:
         print("\t",aa,)
 
-
+#asked the user to choose keys to reach to the final value.
 lang=input("in which language would you like to be spoiled today?")
 if lang in lovewords:
     print("available words:",lovewords[lang])
